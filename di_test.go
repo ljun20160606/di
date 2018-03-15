b74dea4f4a89333d7e25803b07f1ca031b0936a5
ljun20160606 <ljun20160606@gmail.com>
2018-03-15 13:29:51 +0800

    test(di_test.go): di测试类规整

--- di_test.go
+++ di_test.go
@@ -1,50 +1,124 @@
 package di
 
 import (
-	"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-type Bar interface {
-	Values() string
-}
-
-type (
-	BarImpl struct {
-		Value string
-	}
-
-	Foo struct {
-		BarAny *BarImpl `di:"*"`
-		Bar    Bar      `di:"*"`
-		Sex    *string  `di:"*"`
-	}
-)
-
-func (Foo) Init() {
-	fmt.Println("foo")
-}
-
-func (f *BarImpl) Values() string {
-	return f.Value
+func reset() {
+	c := defaultContainer.(*container)
+	c.cupMap = make(map[string][]*Cup)
+	c.iceMap = make(map[string][]Ice)
 }
 
 func TestPut(t *testing.T) {
+	reset()
+
+	type Duck struct {
+		Name *string `di:"*"`
+	}
+
 	ast := assert.New(t)
 
-	foo := new(Foo)
-	bar := &BarImpl{Value: "dc"}
-	sex := "sex"
+	ast.Panics(func() {
+		Put("duck")
+	})
 
-	Put(foo)
-	Put(bar)
+	duck := Duck{}
+
+	Put(&duck)
+
+	ast.NotPanics(func() {
+		duckName := "duck"
+		Put(&duckName)
+
+		Start()
+
+		ast.Equal(&duckName, duck.Name)
+	})
+}
+
+func TestPutWithName(t *testing.T) {
+	reset()
+
+	type Duck struct {
+		Name *string `di:"duckName"`
+	}
+
+	ast := assert.New(t)
+
+	duck := Duck{}
+
+	Put(&duck)
+
+	ast.NotPanics(func() {
+		duckName := "duck"
+		PutWithName(&duckName, "duckName")
+		Start()
+
+		ast.Equal(&duckName, duck.Name)
+	})
+}
+
+func TestGetWithName(t *testing.T) {
+	reset()
+
+	ast := assert.New(t)
+
+	name := "duck"
+
+	Put(&name)
+
+	withName := GetWithName("string")
+
+	ast.Equal(&name, withName)
+}
+
+func TestTomlLoad(t *testing.T) {
+	reset()
+
+	type Duck struct {
+		Name string `di:"#.name"`
+	}
+
+	ast := assert.New(t)
+
+	ast.NoError(TomlLoad(`name = "duck"`))
+
+	duck := Duck{}
+
+	Put(&duck)
+
+	Start()
+
+	ast.Equal("duck", duck.Name)
+}
+
+type Sex struct {
+}
+
+func (s *Sex) BeforeInit() interface{} {
+	return "male"
+}
+
+func TestBeforeInitPlugin(t *testing.T) {
+	reset()
+
+	type Duck struct {
+		Sex    string `di:"@.*"`
+		SexToo string `di:"@.di.Sex"`
+	}
+
+	ast := assert.New(t)
+
+	duck := Duck{}
+	sex := Sex{}
+
+	Put(&duck)
 	Put(&sex)
 
 	Start()
 
-	ast.Equal(bar, foo.BarAny)
-	ast.Equal(bar, foo.Bar)
-	ast.Equal(&sex, foo.Sex)
-	ast.Equal(bar.Value, foo.Bar.Values())
+	ast.Equal(sex.BeforeInit(), duck.SexToo)
+	ast.Equal(duck.Sex, duck.SexToo)
 }
